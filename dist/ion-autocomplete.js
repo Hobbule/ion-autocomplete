a7acaef35f857d35caf997fff430cc4c1bdcd189
Sebastián Rojas Ricaurte <sebastian.rojas.ricaurte@gmail.com>
2016-11-05 18:37:17 -0500

    Back button action

--- dist/ion-autocomplete.js
+++ dist/ion-autocomplete.js
@@ -302,6 +302,12 @@ angular.module('ion-autocomplete', []).directive('ionAutocomplete', ['$ionicScro
 
                         modal.show();
 
+                        // hide the container if the back button is pressed
+                        scope.$deregisterBackButton = $ionicPlatform.registerBackButtonAction(function () {
+                            
+                            ionAutocompleteController.hideModal();
+                        }, 300);
+
                         ionAutocompleteController.fetchSearchQuery("", true);
 
                         // force the collection repeat to redraw itself as there were issues when the first items were added
